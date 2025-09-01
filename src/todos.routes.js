b36d7b45d441a667cf882a8fc4141513c0da31f4
zhishopov <zhishopov@gmail.com>
2025-09-01 13:29:49 +0100

    Add get one todo

--- src/todos.routes.js
+++ src/todos.routes.js
@@ -29,4 +29,21 @@ router.get('/', async (req, res) => {
         console.log(error);
         res.status(500);
     }
+});
+
+// Get One
+router.get('/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if(isNaN(id)) {
+            return res.status(400);
+        }
+
+        const result = await pool.query('SELECT * FROM todos WHERE id = $1;', [id]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
 })
